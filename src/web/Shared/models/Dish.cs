d283320bc7733f999a0ad1de43b717be7303ee9e
Coolastelammetcodes <Martinlam36@gmail.com>
2026-03-11 10:42:06 +0100

    Models, new model Dish

---
+++ src/web/Shared/models/Dish.cs
@@ -0,0 +1,10 @@
+namespace web.Shared.models;
+
+public class Dish
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public int Price { get; set; }
+    public Restaurant? Restaurant { get; set; }
+}
