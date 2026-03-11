7bc1ac3e74a762061708199735af5536ae53cc7b
Coolastelammetcodes <Martinlam36@gmail.com>
2026-03-11 10:53:00 +0100

    Dish, added new column RestaurantID

--- src/web/Shared/models/Dish.cs
+++ src/web/Shared/models/Dish.cs
@@ -6,5 +6,6 @@ public class Dish
     public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
     public int Price { get; set; }
+    public int RestaurantID { get; set; }
     public Restaurant? Restaurant { get; set; }
 }
