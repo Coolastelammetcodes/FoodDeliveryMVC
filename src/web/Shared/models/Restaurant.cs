6d726831fcce38f51f9bf0aa99f564e4ec532e02
Coolastelammetcodes <Martinlam36@gmail.com>
2026-03-11 10:41:52 +0100

    Models, new model Restaurant

---
+++ src/web/Shared/models/Restaurant.cs
@@ -0,0 +1,12 @@
+namespace web.Shared.models;
+
+public class Restaurant
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Address { get; set; } = string.Empty;
+    public string PhoneNum { get; set; } = string.Empty;
+    public string OpenHours { get; set; } = string.Empty;
+    public bool IsOpen { get; set; }
+    public string Description { get; set; } = string.Empty;
+}
