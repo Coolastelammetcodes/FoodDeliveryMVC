7cce8cbf024e6ef949b4a18831ecb3f9bbb4ce47
Coolastelammetcodes <Martinlam36@gmail.com>
2026-03-18 14:19:41 +0100

    Restaurant, added constructor

--- src/domain/entities/Restaurant.cs
+++ src/domain/entities/Restaurant.cs
@@ -3,11 +3,20 @@ namespace domain.entities;
 public class Restaurant
 {
     public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-    public string Address { get; set; } = string.Empty;
-    public string PhoneNum { get; set; } = string.Empty;
-    public string OpenHours { get; set; } = string.Empty;
+    public string Name { get; set; }
+    public string Address { get; set; }
+    public string PhoneNum { get; set; }
+    public string OpenHours { get; set; } 
     public bool IsOpen { get; set; }
-    public string Description { get; set; } = string.Empty;
+    public string Description { get; set; } 
     public List<Dish>? Dishes{ get; set; }
+
+    public Restaurant(string name, string address, string phoneNum, string openHours, string description)
+    {
+        Name = name;
+        Address = address;
+        PhoneNum = phoneNum;
+        OpenHours = openHours;
+        Description = description;
+    }
 }
