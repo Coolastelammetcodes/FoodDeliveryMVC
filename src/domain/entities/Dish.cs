cbb16d0c6a323d3d32017f9ebf6a569b985e5464
Coolastelammetcodes <Martinlam36@gmail.com>
2026-03-18 14:19:12 +0100

    Dish, added constructor in Dish

--- src/domain/entities/Dish.cs
+++ src/domain/entities/Dish.cs
@@ -3,9 +3,15 @@ namespace domain.entities;
 public class Dish
 {
     public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-    public string Description { get; set; } = string.Empty;
+    public string Name { get; set; } 
+    public string Description { get; set; } 
     public int Price { get; set; }
     public int RestaurantID { get; set; }
     public Restaurant? Restaurant { get; set; }
+
+    public Dish(string name, string description)
+    {
+        Name = name;
+        Description = description;
+    }
 }
