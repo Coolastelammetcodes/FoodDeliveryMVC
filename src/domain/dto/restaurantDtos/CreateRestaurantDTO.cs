17f273c10be26087079459ea63573f164d2c1ee6
Coolastelammetcodes <Martinlam36@gmail.com>
2026-03-23 15:59:38 +0100

    CreateRestaurantDTO, changed to dataAnnotation.Requiered attribute

--- src/domain/dto/restaurantDtos/CreateRestaurantDTO.cs
+++ src/domain/dto/restaurantDtos/CreateRestaurantDTO.cs
@@ -1,9 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 public class CreateRestaurantDTO
 {
-    public required string Name { get; set; } 
-    public required string Address { get; set; } 
-    public required string PhoneNum { get; set; } 
+    [Required]
+    public string Name { get; set; } = string.Empty;
+    [Required] 
+    public string Address { get; set; } = string.Empty;
+    [Required] 
+    public string PhoneNum { get; set; } = string.Empty;
     public TimeSpan Open { get; set; }
     public TimeSpan Closed { get; set; }
-    public required string Description { get; set; } 
+    [Required]
+    public string Description { get; set; } = string.Empty; 
 }
