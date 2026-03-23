f1e6a760ce851ab922f2f4caaaab1e096aae7c73
Coolastelammetcodes <Martinlam36@gmail.com>
2026-03-23 15:33:56 +0100

    RestaurantService, mapped CreateRestaurantDTO to Restaurant entity

--- src/domain/services/RestaurantService.cs
+++ src/domain/services/RestaurantService.cs
@@ -3,10 +3,24 @@ using domain.interfaces;
 
 public class RestaurantService
 {
-    private readonly IRestaurantRepository _restaurant;
-    public RestaurantService(IRestaurantRepository restaurant)
+    private readonly IRestaurantRepository _restaurantrepo;
+    public RestaurantService(IRestaurantRepository restaurantrepo)
     {
-        _restaurant = restaurant;
+        _restaurantrepo = restaurantrepo;
     }
     
+    public async Task AddRestaurantAsync(CreateRestaurantDTO dto)
+    {
+        var restaurant = new Restaurant
+        (
+            dto.Name,
+            dto.Address,
+            dto.PhoneNum,
+            dto.Open,
+            dto.Closed,
+            dto.Description
+        );
+
+        await _restaurantrepo.AddNewRestaurantAsync(restaurant);
+    }
 }
