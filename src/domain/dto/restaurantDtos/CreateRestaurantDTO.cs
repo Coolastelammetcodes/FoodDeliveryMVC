d568ccbb172232af09efe5a8fc9ac194b5dba68a
Coolastelammetcodes <Martinlam36@gmail.com>
2026-03-24 16:07:38 +0100

    CreateRestaurantDTO, Changed so that Description is not requiered

--- src/domain/dto/restaurantDtos/CreateRestaurantDTO.cs
+++ src/domain/dto/restaurantDtos/CreateRestaurantDTO.cs
@@ -8,8 +8,8 @@ public class CreateRestaurantDTO
     public string Address { get; set; } = string.Empty;
     [Required] 
     public string PhoneNum { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty; 
     public TimeSpan Open { get; set; }
     public TimeSpan Closed { get; set; }
-    [Required]
-    public string Description { get; set; } = string.Empty; 
+    
 }
