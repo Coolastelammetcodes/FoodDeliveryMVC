fac1f0fda300849fbdd656bace906096f98076fc
Coolastelammetcodes <Martinlam36@gmail.com>
2026-03-25 18:53:10 +0100

    program in api, adde DI to restaurantService, IrestaurantRepo and RestaurantRepo

--- src/webApi/Program.cs
+++ src/webApi/Program.cs
@@ -1,12 +1,16 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using infrastructure.data;
+using domain.interfaces;
+using infrastructure.repositories;
 
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddDbContext<FoodServiceContext>(options => options.UseInMemoryDatabase("TestDb"));
 // Add services to the container.
-builder.Services.AddIdentity<IdentityUser, IdentityRole>();
+builder.Services.AddScoped<RestaurantService>();
+builder.Services.AddScoped<IRestaurantRepository, RestaurantRepository>();
+builder.Services.AddDbContext<FoodServiceContext>(options => options.UseInMemoryDatabase("TestDb"));
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<FoodServiceContext>();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
@@ -17,5 +21,7 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+
+app.MapControllers();
 
 app.Run();
