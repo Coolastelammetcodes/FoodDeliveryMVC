85768eb89ad8be97bfde7be8d705f1ea5ea50c0e
Coolastelammetcodes <Martinlam36@gmail.com>
2026-03-25 13:49:45 +0100

    FoodServiceContext, changed to IdentityDbContext instead of regular DbContext

--- src/infrastructure/data/FoodServiceContext.cs
+++ src/infrastructure/data/FoodServiceContext.cs
@@ -1,8 +1,10 @@
 using Microsoft.EntityFrameworkCore;
 using domain.entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 namespace infrastructure.data;
 
-public class FoodServiceContext : DbContext
+public class FoodServiceContext : IdentityDbContext<IdentityUser>
 {
     public FoodServiceContext(DbContextOptions<FoodServiceContext> options) : base(options) {}
     public DbSet<Restaurant> Restaurants { get; set; }
