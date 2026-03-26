40d43a58ae7f3220544a145aa034942b1674be79
Coolastelammetcodes <Martinlam36@gmail.com>
2026-03-26 14:06:19 +0100

    Added Authorize in  Controller

--- src/web/controllers/RestaurantController.cs
+++ src/web/controllers/RestaurantController.cs
@@ -1,15 +1,19 @@
 using System.Runtime.Versioning;
+using infrastructure.data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 [ApiController]
-[Route ("api/[controller]")]
+[Route ("api/[controller]")] //TODO Fixa resten av REST API
+[Authorize]
 public class RestaurantController : ControllerBase
 {
     private readonly RestaurantService _restaurantService;
-
-    public RestaurantController(RestaurantService restaurantService)
+    private readonly FoodServiceContext _dbContext;
+    public RestaurantController(RestaurantService restaurantService, FoodServiceContext dbContext)
     {
         _restaurantService = restaurantService;
+        _dbContext = dbContext;
     }
 
     [HttpPost]
