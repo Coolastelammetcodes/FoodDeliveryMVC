3a682f744e97a0a54e221c793dc33aac67b72b5d
Coolastelammetcodes <Martinlam36@gmail.com>
2026-03-27 13:00:05 +0100

    Program, changed database to a temporary inmemory. Added DI for DbInitializer and CreateAsyncScope to be able to find my seed restaurants via Initialize

--- src/web/Program.cs
+++ src/web/Program.cs
@@ -3,25 +3,34 @@ using infrastructure.data;
 using infrastructure.repositories;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.Extensions.FileSystemGlobbing.Internal.Patterns;
 using web.Data;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-builder.Services.AddDbContext<FoodServiceContext>(options =>
-    options.UseSqlite(connectionString));
+// var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+var db = builder.Services.AddDbContext<FoodServiceContext>(options =>
+    options.UseInMemoryDatabase("FoodServiceDB"));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddScoped<IRestaurantRepository, RestaurantRepository>();
 builder.Services.AddScoped<IRestaurantService, RestaurantService>();
+builder.Services.AddScoped<DbInitializer>();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<FoodServiceContext>();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateAsyncScope())
+{
+    var dbInit = scope.ServiceProvider.GetRequiredService<DbInitializer>();
+    await dbInit.InitializeAsync();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
