6d354e6a7cd713eb8f858d785636f8cca28b85b8
Coolastelammetcodes <Martinlam36@gmail.com>
2026-03-27 12:56:58 +0100

    Dbinitializer, constructor and using a list to collect all seedrestaurants in one variable

--- src/infrastructure/data/DbInitializer.cs
+++ src/infrastructure/data/DbInitializer.cs
@@ -1,33 +1,46 @@
 using domain.entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace infrastructure.data;
 public class DbInitializer
 {
-    public async Task Initialize(FoodServiceContext db)
+    private readonly FoodServiceContext _db;
+    public DbInitializer(FoodServiceContext db)
     {
-        await SeedRestaurants(db);
+        _db = db;
     }
-    public async Task SeedRestaurants(FoodServiceContext db)
+    public async Task InitializeAsync()
     {
-        var restaurant1 = new Restaurant 
-        (   
-            "Bob's Burgers",
-            "Ocean Avenue",
-            "Great burgers only at Bobs Burgers",
-            new TimeSpan(09,00,0),
-            new TimeSpan(21,00,0),
-            new TimeSpan(20,45,0)
-        );
-         var restaurant2 = new Restaurant 
-        (   
-            "Krusty Burger",
-            "Fast Food Boulevard",
-            "Fun burgers",
-            new TimeSpan(11,00,0),
-            new TimeSpan(21,30,0),
-            new TimeSpan(21,15,0)
-        );
-        await db.Restaurants.AddRangeAsync(restaurant1, restaurant2);
-        await db.SaveChangesAsync();
+       await SeedRestaurantAsync();
+    }
+    public async Task SeedRestaurantAsync()
+    {
+        if(!await _db.Restaurants.AnyAsync())
+        {
+            var restaurants = new List<Restaurant>
+            {
+                new Restaurant(   
+                    "Bob's Burgers",
+                    "Ocean Avenue",
+                    "Great burgers only at Bobs Burgers",
+                    new TimeSpan(09,00,0),
+                    new TimeSpan(21,00,0),
+                    new TimeSpan(20,45,0)
+                ),
+                new Restaurant 
+                (   
+                    "Krusty Burger",
+                    "Fast Food Boulevard",
+                    "Fun burgers",
+                    new TimeSpan(11,00,0),
+                    new TimeSpan(21,30,0),
+                    new TimeSpan(21,15,0)
+                )  
+            }; 
+             
+            await _db.Restaurants.AddRangeAsync(restaurants);
+            await _db.SaveChangesAsync();
+        }
+       
     }
 }
