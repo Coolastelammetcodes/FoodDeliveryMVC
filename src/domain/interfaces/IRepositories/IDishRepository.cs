95cc39c496e9b108f6fb06d7d060e4a315731161
Coolastelammetcodes <Martinlam36@gmail.com>
2026-03-28 22:53:46 +0100

    IDishRepo and DishRepo, new method for showing all the relevant dishes for specific restaurant

--- src/domain/interfaces/IRepositories/IDishRepository.cs
+++ src/domain/interfaces/IRepositories/IDishRepository.cs
@@ -4,6 +4,7 @@ using domain.entities;
 public interface IDishRepository
 {
     Task<List<Dish>> ViewAllDishesAsync();
+    Task<List<Dish>> GetDishesForRestaurantAsync(int restaurantID);
     Task<Dish?> ViewSpecificDishAsync(int id);
     Task AddNewDishAsync(Dish dish);
 }
--- src/infrastructure/repositories/DishRepository.cs
+++ src/infrastructure/repositories/DishRepository.cs
@@ -9,6 +9,7 @@ public class DishRepository : IDishRepository
     {
         _dbContext = dbContext;
     }
+    public async Task<List<Dish>> GetDishesForRestaurantAsync(int restaurantID) => await _dbContext.Dishes.Where(d => d.RestaurantID == restaurantID).ToListAsync();
     public async Task<List<Dish>> ViewAllDishesAsync() => await _dbContext.Dishes.ToListAsync(); 
     public async Task<Dish?> ViewSpecificDishAsync(int id) => await _dbContext.Dishes.FindAsync(id);
     public async Task AddNewDishAsync(Dish dish)
