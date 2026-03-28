289f375fadfe339d1af859a45a8aeb2a3550d418
Coolastelammetcodes <Martinlam36@gmail.com>
2026-03-28 22:56:53 +0100

    Comment

--- src/web/Controllers/DishController.cs
+++ src/web/Controllers/DishController.cs
@@ -1,19 +1,19 @@
 using Microsoft.AspNetCore.Mvc;
 
-public class DishController : Controller
+public class DishController : Controller //TODO lägg till beskrivning till rätten 
 {
     private readonly DishService _dishService;
     public DishController(DishService dishService)
     {
         _dishService = dishService;
     }
-    public async Task<IActionResult> Details(int id)
-    {
-        var dish = await _dishService.ViewSpecificDishAsync(id);
-        if(dish == null)
-        {
-            return NotFound();
-        }
-        return View(dish);
-    }
+    // public async Task<IActionResult> Details(int id)
+    // {
+    //     // var dish = await _dishService.ViewSpecificDishAsync(id);
+    //     // if(dish == null)
+    //     // {
+    //     //     return NotFound();
+    //     // }
+    //     // return View(dish);
+    // }
 }
