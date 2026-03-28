43265f416bcc885b4ec1a70b64b22a720cbec3d8
Coolastelammetcodes <Martinlam36@gmail.com>
2026-03-28 10:29:55 +0100

    DishController, added Details controller

---
+++ src/web/Controllers/DishController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+
+public class DishController : Controller
+{
+    private readonly DishService _dishService;
+    public DishController(DishService dishService)
+    {
+        _dishService = dishService;
+    }
+    public async Task<IActionResult> Details(int id)
+    {
+        var dish = await _dishService.ViewSpecificDishAsync(id);
+        if(dish == null)
+        {
+            return NotFound();
+        }
+        return View(dish);
+    }
+}
