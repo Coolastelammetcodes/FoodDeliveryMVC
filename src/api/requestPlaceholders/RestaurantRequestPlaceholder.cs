6f0791f17a9f7a70aee1b5539dae357166d34bec
Coolastelammetcodes <Martinlam36@gmail.com>
2026-03-30 16:47:23 +0200

    RestaurantRequestPlaceHolder, added placeholders

--- src/api/requestPlaceholders/RestaurantRequestPlaceholder.cs
+++ src/api/requestPlaceholders/RestaurantRequestPlaceholder.cs
@@ -1,6 +1,19 @@
+using domain.dto;
 using Microsoft.OpenApi;
+using Swashbuckle.AspNetCore.Filters;
 
-public class RestaurantRequestPlaceholder
+public class RestaurantRequestPlaceholder : IExamplesProvider<RestaurantRequestDTO>
 {
-    
+    public RestaurantRequestDTO GetExamples()
+    {
+        return new RestaurantRequestDTO
+        {
+            Name = "Restaurangens namn",
+            Address = "Gata, stad",
+            Description = "Kort beskrivning om restaurangen",
+            Open = new TimeSpan(09,00,0),
+            Closed = new TimeSpan(21,00,0),
+            OrderDeadline = new TimeSpan(20,45,0)
+        };
+    }
 }
