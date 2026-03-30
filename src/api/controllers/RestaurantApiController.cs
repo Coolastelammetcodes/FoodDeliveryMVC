37536fcf3a752fcaf5dda65643e3761dfa51ff5f
Coolastelammetcodes <Martinlam36@gmail.com>
2026-03-30 12:49:29 +0200

    RestaurantApiController, new api controller for adding restaurant

--- src/api/controllers/RestaurantApiController.cs
+++ src/api/controllers/RestaurantApiController.cs
@@ -1,6 +1,19 @@
+using domain.dto;
 using Microsoft.AspNetCore.Mvc;
+using domain.interfaces;
 [ApiController]
+[Route("api/[controller]")]
 public class RestaurantApiController : ControllerBase
 {
-    
+    private readonly IRestaurantService _restaurantService;
+    public RestaurantApiController(IRestaurantService restaurantService)
+    {
+        _restaurantService = restaurantService;
+    }
+    [HttpPost]
+    public async Task<IActionResult> AddRestaurant(RestaurantRequestDTO dto)
+    {
+        await _restaurantService.AddRestaurantAsync(dto);
+        return Created();
+    }
 }
