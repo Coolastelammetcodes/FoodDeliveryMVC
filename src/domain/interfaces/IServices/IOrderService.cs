9007fe6b71cf3fcc60d4fc39eb3a84a224b43fdf
Coolastelammetcodes <Martinlam36@gmail.com>
2026-03-31 20:06:34 +0200

    IOrderService, ViewSpecificOrder

--- src/domain/interfaces/IServices/IOrderService.cs
+++ src/domain/interfaces/IServices/IOrderService.cs
@@ -3,4 +3,5 @@ using domain.entities;
 public interface IOrderService
 {
     Task<OrderResponseDTO> AddNewOrderAsync(OrderRequestDTO orderDTO);
+    Task<OrderResponseDTO?> ViewSpecificOrderAsync(Guid Id);
 }
