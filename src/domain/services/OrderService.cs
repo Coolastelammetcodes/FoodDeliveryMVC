19cee87761bf0638584338cb997a0203354a69fa
Coolastelammetcodes <Martinlam36@gmail.com>
2026-03-31 14:22:31 +0200

    OrderService, new method for adding new order using OrderRepo and the helper-classes below

--- src/domain/services/OrderService.cs
+++ src/domain/services/OrderService.cs
@@ -1,13 +1,24 @@
 using domain.entities;
+using domain.interfaces;
 
-public class OrderService 
-{
+public class OrderService : IOrderService
+{   
+    private readonly IOrderRepository _orderRepo;
     private readonly OrderItemService _orderItemService;
-    public OrderService(OrderItemService orderItemService)
+    public OrderService(IOrderRepository orderRepo, OrderItemService orderItemService)
     {
+        _orderRepo = orderRepo;
         _orderItemService = orderItemService;
     }
-    
+    public async Task<OrderResponseDTO> AddNewOrderAsync(OrderRequestDTO reqDto)
+    {
+        var order = new Order
+        {
+            OrderItems = reqDto.OrderItems.Select(MapToOrderItem).ToList()
+        };
+        await _orderRepo.AddNewOrderAsync(order);
+        return MapToOrderResponseDTO(order);
+    }
     private OrderItem MapToOrderItem(OrderItemRequestDTO oi) => new OrderItem (oi.DishID, oi.Quantity);
 
     private OrderResponseDTO MapToOrderResponseDTO(Order o) => new OrderResponseDTO(Id: o.Id, OrderItems: o.OrderItems.Select(oi => new OrderItemResponseDTO(oi.Id, oi.DishID, oi.Quantity)).ToList());
