26f1c06725758aeddbf6c33f25dc9248dac17c2f
Coolastelammetcodes <Martinlam36@gmail.com>
2026-03-31 13:33:36 +0200

    OrderService, two helper-methods for mapping to OrderItem and OrderResponseDTO

--- src/domain/services/OrderService.cs
+++ src/domain/services/OrderService.cs
@@ -1,4 +1,15 @@
-public class OrderService // TODO fortsätt bygga här efter OrderItem
+using domain.entities;
+
+public class OrderService 
 {
+    private readonly OrderItemService _orderItemService;
+    public OrderService(OrderItemService orderItemService)
+    {
+        _orderItemService = orderItemService;
+    }
     
+    private OrderItem MapToOrderItem(OrderItemRequestDTO oi) => new OrderItem (oi.DishID, oi.Quantity);
+
+    private OrderResponseDTO MapToOrderResponseDTO(Order o) => new OrderResponseDTO(Id: o.Id, OrderItems: o.OrderItems.Select(oi => new OrderItemResponseDTO(oi.Id, oi.DishID, oi.Quantity)).ToList());
+
 }
