03653a25e343568a846dd5abe1da524d6e145ef2
Coolastelammetcodes <Martinlam36@gmail.com>
2026-03-31 08:41:01 +0200

    OrderItemRepo, a method For adding order items. Also added validation in case user types in anything less than one in quantity of dish

---
+++ src/domain/services/OrderItemService.cs
@@ -0,0 +1,25 @@
+using domain.entities;
+using domain.interfaces;
+
+public class OrderItemService : IOrderItemService
+{
+    private readonly IOrderItemRepository _orderItemRepo;
+    public OrderItemService(IOrderItemRepository orderItemRepo)
+    {
+        _orderItemRepo = orderItemRepo;
+    }
+    public async Task AddOrderItemAsync(OrderItemRequestDTO dto)
+    {
+        if(dto.Quantity < 1)
+        {
+            throw new Exception("Det måste vara minst 1 antal");
+        }
+        var orderItem = new OrderItem
+        (
+            dto.DishID,
+            dto.Quantity
+        );
+
+        await _orderItemRepo.AddOrderItemAsync(orderItem);
+    }
+}
