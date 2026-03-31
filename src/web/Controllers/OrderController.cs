2b6cee508067d057242b65bc42a3789e841c3a78
Coolastelammetcodes <Martinlam36@gmail.com>
2026-03-31 15:41:47 +0200

    OrderController, new post controller for adding order and a get controller to get order-id and dishes that has been chosen.

--- src/web/Controllers/OrderController.cs
+++ src/web/Controllers/OrderController.cs
@@ -8,4 +8,15 @@ public class OrderController : Controller
         _orderService = orderService;
     }
     
+    [HttpPost]
+    public async Task<IActionResult> AddOrder(OrderRequestDTO reqDto)
+    {
+        var order = await _orderService.AddNewOrderAsync(reqDto);
+        return RedirectToAction("Confirmation", new { id = order.Id});
+    }
+    [HttpGet]
+    public async Task<IActionResult> Confirmation(Guid id)
+    {
+        
+    }
 }
