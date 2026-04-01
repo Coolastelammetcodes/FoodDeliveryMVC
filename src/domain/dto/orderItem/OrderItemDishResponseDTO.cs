79bd930e029f668365597ecfb2d5ccaa2e113345
Coolastelammetcodes <Martinlam36@gmail.com>
2026-04-02 00:22:08 +0200

    OrderItemDishResponseDTO, changed from set to init on all properites and added new prop for Price

--- src/domain/dto/orderItem/OrderItemDishResponseDTO.cs
+++ src/domain/dto/orderItem/OrderItemDishResponseDTO.cs
@@ -1,6 +1,18 @@
+namespace domain.dto;
 public class OrderItemDishResponseDTO
 {
     public int Id { get; init; }
-    public int DishID { get; set; }
-    public string DishName { get; set; } = string.Empty;
+    public int DishID { get; init; }
+    public int Quantity { get; init;}
+    public string DishName { get; init; } = string.Empty;
+    public int Price { get; init; } // TODO byt namn till DishPrice
+
+    public OrderItemDishResponseDTO(int id, int dishID, int quantity, string dishName, int price)
+    {
+        Id = id;
+        DishID = dishID;
+        Quantity = quantity;
+        DishName = dishName;
+        Price = price;
+    }
 }
