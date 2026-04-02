56e25395c56b8f032a65c11b62323dd3716d8c58
Coolastelammetcodes <Martinlam36@gmail.com>
2026-04-02 10:50:40 +0200

    Order, added new properties. One for user to write instructions to Deliverer. And one navigation property to Customer

--- src/domain/entities/Order.cs
+++ src/domain/entities/Order.cs
@@ -3,4 +3,8 @@ public class Order
 {
     public Guid Id { get; init; } = Guid.NewGuid(); 
     public List<OrderItem> OrderItems { get; set; } = new();
+    public string Instructions { get; set; } = string.Empty;
+    public Customer Customer { get; set; } = new(); 
+
+    // TODO lägg props för att mata in kunduppgifter här och skippa Customer klassen om inte du hinner göra färdigt Customer innan lördag
 }
