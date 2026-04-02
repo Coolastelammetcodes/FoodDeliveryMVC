6cda77d31bf6f3a1314476e4f1595cb642a861ee
Coolastelammetcodes <Martinlam36@gmail.com>
2026-04-02 11:46:11 +0200

    FoodServiceContext. added Customer class in database as a database-table

--- src/infrastructure/data/FoodServiceContext.cs
+++ src/infrastructure/data/FoodServiceContext.cs
@@ -11,4 +11,5 @@ public class FoodServiceContext : IdentityDbContext<IdentityUser>
     public DbSet<Dish> Dishes { get; set; }
     public DbSet<Order> Orders { get; set; }
     public DbSet<OrderItem> OrderItems { get; set; }
+    public DbSet<Customer> Customers { get; set; }
 }
