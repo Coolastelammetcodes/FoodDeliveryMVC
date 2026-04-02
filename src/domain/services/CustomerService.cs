5fa643311cd7c9a34cce9f27cddfa0b8cf4f7704
Coolastelammetcodes <Martinlam36@gmail.com>
2026-04-02 16:52:12 +0200

    CustomerService, linked dto props and entity props in method AddCustomer.

--- src/domain/services/CustomerService.cs
+++ src/domain/services/CustomerService.cs
@@ -1,7 +1,21 @@
+using domain.interfaces;
+
 public class CustomerService : ICustomerService
 {
-    public async Task AddCustomer(CustomerRequestDTO customerDto)
+    private readonly ICustomerRepository _customerRepo;
+    public CustomerService(ICustomerRepository customerRepo)
     {
-        
+        _customerRepo = customerRepo;
     }
+    public async Task AddCustomerAsync(CustomerRequestDTO dto)
+    {
+        var customer = new Customer
+        {
+            Name = dto.Name,
+            PhoneNum = dto.PhoneNum,
+            Email = dto.Email
+        };
+        await _customerRepo.AddCustomerAsync(customer);
+    }
+
 }
