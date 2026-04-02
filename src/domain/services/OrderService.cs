d8c09a0a3715fc652a775bda5d54f4a81cad46df
Coolastelammetcodes <Martinlam36@gmail.com>
2026-04-02 16:51:11 +0200

    OrderService, deleted empty rows

--- src/domain/services/OrderService.cs
+++ src/domain/services/OrderService.cs
@@ -30,7 +30,5 @@ public class OrderService : IOrderService
         return MapToOrderResponseDTO(order);
     }
     private OrderItem MapToOrderItem(OrderItemRequestDTO oi) => new OrderItem (oi.DishID, oi.Quantity);
-
     private OrderResponseDTO MapToOrderResponseDTO(Order o) => new OrderResponseDTO{ Id = o.Id, OrderItems = o.OrderItems.Select(oi => new OrderItemDishResponseDTO(oi.Id, oi.DishID, oi.Quantity, oi.Dish?.Name ?? "", oi.Dish?.Price ?? 0)).ToList()};
-
 }
