b867a50c7351bbbb703379a148cf8c4607d7538e
Coolastelammetcodes <Martinlam36@gmail.com>
2026-04-02 11:30:09 +0200

    CustomerRequestDTO, properties and used dataannotation-attribute to set required properties

---
+++ src/domain/dto/customer/CustomerRequestDTO.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+using domain.dto;
+
+public class CustomerRequestDTO
+{
+    [Required]
+    public string Name { get; set; } = string.Empty;
+    [Required]
+    public string PhoneNum { get; set; } = string.Empty;
+    [Required]
+    public string Mail { get; set; } = string.Empty;
+    public List<OrderResponseDTO?> Orders { get; set; } = new();
+}
