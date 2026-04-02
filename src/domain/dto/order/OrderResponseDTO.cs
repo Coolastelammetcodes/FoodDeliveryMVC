76e2c21653330c4add39e045e9cf4ff192a4d191
Coolastelammetcodes <Martinlam36@gmail.com>
2026-04-02 11:42:47 +0200

    OrderResponseDTO, properties related to customer

--- src/domain/dto/order/OrderResponseDTO.cs
+++ src/domain/dto/order/OrderResponseDTO.cs
@@ -2,5 +2,7 @@ namespace domain.dto;
 public record class OrderResponseDTO()
 {
     public Guid Id { get; init;}
-    public List<OrderItemDishResponseDTO> OrderItems { get; init; } = new(); 
+    public List<OrderItemDishResponseDTO> OrderItems { get; init; } = new();
+    public int CustomerID { get; init; } 
+    public Customer Customer { get; init; } = new(); 
 }
