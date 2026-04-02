a30581c67bc0aab7bb3ff637c3dad2d7d301b2ea
Coolastelammetcodes <Martinlam36@gmail.com>
2026-04-02 11:44:58 +0200

    Order, added prop for taking in customer-id

--- src/domain/entities/Order.cs
+++ src/domain/entities/Order.cs
@@ -4,6 +4,7 @@ public class Order
     public Guid Id { get; init; } = Guid.NewGuid(); 
     public List<OrderItem> OrderItems { get; set; } = new();
     public string Instructions { get; set; } = string.Empty;
+    public int CustomerID { get; set; }
     public Customer Customer { get; set; } = new(); 
 
     // TODO lägg props för att mata in kunduppgifter här och skippa Customer klassen om inte du hinner göra färdigt Customer innan lördag
