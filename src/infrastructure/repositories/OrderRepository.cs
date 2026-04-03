ca347cd45703a97f64d354c56b798c386be90041
Coolastelammetcodes <Martinlam36@gmail.com>
2026-04-03 11:09:55 +0200

    OrderRepo, included customer

--- src/infrastructure/repositories/OrderRepository.cs
+++ src/infrastructure/repositories/OrderRepository.cs
@@ -15,5 +15,5 @@ public class OrderRepository : IOrderRepository
         await _db.Orders.AddAsync(order);
         await _db.SaveChangesAsync();
     }
-    public async Task<Order?> ViewSpecificOrderWithDishesAsync(Guid Id) => await _db.Orders.Include(o => o.OrderItems).ThenInclude(oi => oi.Dish).FirstOrDefaultAsync(o => o.Id == Id);
+    public async Task<Order?> ViewSpecificOrderWithDishesAsync(Guid Id) => await _db.Orders.Include(o => o.Customer).Include(o => o.OrderItems).ThenInclude(oi => oi.Dish).FirstOrDefaultAsync(o => o.Id == Id);
 }
