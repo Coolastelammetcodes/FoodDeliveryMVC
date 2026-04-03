53859d570a1daba17993d08cfac00d3937fd29ab
Coolastelammetcodes <Martinlam36@gmail.com>
2026-04-03 10:57:36 +0200

    CustomerResponseDTO, changed type to Guid on Id

--- src/domain/dto/customer/CustomerResponseDTO.cs
+++ src/domain/dto/customer/CustomerResponseDTO.cs
@@ -3,10 +3,9 @@ using domain.entities;
 
 public record class CustomerResponseDTO
 {
-    public int Id { get; init; }
+    public Guid Id { get; init; }
     public string Name { get; init; } = string.Empty;
     public string PhoneNum { get; init; } = string.Empty;
     public string Email{ get; init; } = string.Empty;
     public List<OrderResponseDTO?> Orders { get; init; } = new();
-
 }
