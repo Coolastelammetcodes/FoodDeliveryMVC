7920d7ef5ab7a124a13b4b25b3f996fdba6a255d
Coolastelammetcodes <Martinlam36@gmail.com>
2026-04-03 11:02:01 +0200

    OrderResponseDTO, only using navprop to CustomerResponseDTO to get customer information

--- src/domain/dto/order/OrderResponseDTO.cs
+++ src/domain/dto/order/OrderResponseDTO.cs
@@ -3,6 +3,5 @@ public record class OrderResponseDTO()
 {
     public Guid Id { get; init;}
     public List<OrderItemDishResponseDTO> OrderItems { get; init; } = new();
-    public int CustomerID { get; init; } 
-    public Customer Customer { get; init; } = new(); 
+    public CustomerResponseDTO Customer { get; init; }
 }
