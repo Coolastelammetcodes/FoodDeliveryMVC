42ac9a4d7108ee7789ce79fd2080ef1d29813d5a
Coolastelammetcodes <Martinlam36@gmail.com>
2026-04-03 11:08:48 +0200

    OrderService, removed orderitem service from constructor

--- src/domain/services/OrderService.cs
+++ src/domain/services/OrderService.cs
@@ -5,17 +5,17 @@ using domain.dto;
 public class OrderService : IOrderService
 {   
     private readonly IOrderRepository _orderRepo;
-    private readonly IOrderItemService _orderItemService;
-    public OrderService(IOrderRepository orderRepo, IOrderItemService orderItemService)
+    public OrderService(IOrderRepository orderRepo)
     {
         _orderRepo = orderRepo;
-        _orderItemService = orderItemService;
     }
     public async Task<OrderResponseDTO> AddNewOrderAsync(OrderRequestDTO reqDto)
     {
         var order = new Order
         {
-            OrderItems = reqDto.OrderItems.Select(MapToOrderItem).ToList()
+            Customer = MapToCustomer(reqDto.Customer),
+            OrderItems = reqDto.OrderItems.Select(MapToOrderItem).ToList(),
+            Instructions = reqDto.Instructions
         };
         await _orderRepo.AddNewOrderAsync(order);
         return MapToOrderResponseDTO(order);
@@ -29,6 +29,11 @@ public class OrderService : IOrderService
         }
         return MapToOrderResponseDTO(order);
     }
+    private Customer MapToCustomer(CustomerRequestDTO c) => new Customer {Name = c.Name, PhoneNum = c.PhoneNum, Email = c.Email};
+    private CustomerResponseDTO MapToCustomerResponse(Customer c) => new CustomerResponseDTO{Id = c.Id, Name = c.Name, Email = c.Email, PhoneNum = c.PhoneNum}; 
     private OrderItem MapToOrderItem(OrderItemRequestDTO oi) => new OrderItem (oi.DishID, oi.Quantity);
-    private OrderResponseDTO MapToOrderResponseDTO(Order o) => new OrderResponseDTO{ Id = o.Id, OrderItems = o.OrderItems.Select(oi => new OrderItemDishResponseDTO(oi.Id, oi.DishID, oi.Quantity, oi.Dish?.Name ?? "", oi.Dish?.Price ?? 0)).ToList()};
+    private OrderResponseDTO MapToOrderResponseDTO(Order o)
+    {
+        return new OrderResponseDTO{ Id = o.Id, Customer = o.Customer != null ? MapToCustomerResponse(o.Customer) : null, OrderItems = o.OrderItems.Select(oi => new OrderItemDishResponseDTO(oi.Id, oi.DishID, oi.Quantity, oi.Dish?.Name ?? "", oi.Dish?.Price ?? 0)).ToList()};
+    }    
 }
