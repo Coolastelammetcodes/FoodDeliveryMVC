d189ecb86ea1f283f933eb51e60d23fe9841ef9f
Coolastelammetcodes <Martinlam36@gmail.com>
2026-04-03 16:27:43 +0200

    OrderItemDishResponseDTO, deleted constructor

--- src/domain/dto/orderItem/OrderItemDishResponseDTO.cs
+++ src/domain/dto/orderItem/OrderItemDishResponseDTO.cs
@@ -6,13 +6,4 @@ public class OrderItemDishResponseDTO
     public int Quantity { get; init;}
     public string DishName { get; init; } = string.Empty;
     public int DishPrice { get; init; } 
-
-    public OrderItemDishResponseDTO(int id, int dishID, int quantity, string dishName, int dishPrice)
-    {
-        Id = id;
-        DishID = dishID;
-        Quantity = quantity;
-        DishName = dishName;
-        DishPrice = dishPrice;
-    }
 }
