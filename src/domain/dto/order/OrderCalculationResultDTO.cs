3ce89d0c7fbc6d1bce9e4f65b84771d2b34ae27e
Coolastelammetcodes <Martinlam36@gmail.com>
2026-04-05 01:10:59 +0200

    OrderCalculationResultDTO, new dto with list of orderitems and prop for totalprice which both will be used for the calculation of totalprice and mapping later in OrderService

---
+++ src/domain/dto/order/OrderCalculationResultDTO.cs
@@ -0,0 +1,8 @@
+using domain.dto;
+using domain.entities;
+
+public class OrderCalculationResultDTO
+{
+    public List<OrderItem> OrderItems { get; set; } = new();
+    public decimal TotalPrice { get; set; }
+}
