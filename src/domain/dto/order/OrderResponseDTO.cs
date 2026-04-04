b5b90a53df29a58840b3c75bf18f5ed0dacf1718
Coolastelammetcodes <Martinlam36@gmail.com>
2026-04-05 01:08:55 +0200

    OrderResponseDTO, added prop for instructions

--- src/domain/dto/order/OrderResponseDTO.cs
+++ src/domain/dto/order/OrderResponseDTO.cs
@@ -4,5 +4,6 @@ public record class OrderResponseDTO()
     public Guid Id { get; init;}
     public List<OrderItemDishResponseDTO> OrderItems { get; init; } = new();
     public CustomerResponseDTO Customer { get; init; } = new();
+    public string Instructions { get; init; } = string.Empty;
     public decimal TotalPrice { get; init; }
 }
