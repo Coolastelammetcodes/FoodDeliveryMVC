c82074db8e81128ff03a9b162f0ee6b91a0e5329
Coolastelammetcodes <Martinlam36@gmail.com>
2026-04-05 14:01:20 +0200

    OrderStatusEnum, created enum-class for orderstatus

--- src/domain/enums/OrderStatusEnum.cs
+++ src/domain/enums/OrderStatusEnum.cs
@@ -1,1 +1,12 @@
+public enum OrderStatusEnum
+{
+    Received = 1,
+    Confirmed = 2,
+    CourierAccepted = 3,
+    Preparing = 4,
+    ReadyForPickup = 5,
+    InTransit = 6,
+    Delivered = 7
+}
 //TODO bygg här efter lunch för att man ska kunna se orderstatus på orderi Confirmation
+       
