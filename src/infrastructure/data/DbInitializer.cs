aca65a287717f0fc9c0e33a26e780443cdb6e0eb
Coolastelammetcodes <Martinlam36@gmail.com>
2026-04-05 21:33:33 +0200

    DbInitializer, new method for creating seed-orders

--- src/infrastructure/data/DbInitializer.cs
+++ src/infrastructure/data/DbInitializer.cs
@@ -1,4 +1,5 @@
 using domain.entities;
+using domain.enums;
 using Microsoft.EntityFrameworkCore;
 
 namespace infrastructure.data;
@@ -13,6 +14,7 @@ public class DbInitializer
     {
        await SeedRestaurantAsync();
        await SeedDishAsync();
+       await SeedOrdersAsync();
     }
     public async Task SeedRestaurantAsync()
     {
@@ -70,5 +72,39 @@ public class DbInitializer
             await _db.Dishes.AddRangeAsync(dishes);
             await _db.SaveChangesAsync();
         }
+    }
+    public async Task SeedOrdersAsync()
+    {
+        var orders = new List<Order>
+        {
+            new Order
+           {
+                Customer = new Customer { Name = "Alice", Email = "alice@mail.com", PhoneNum = "0701234567" },
+                Instructions = "Ingen lök tack",
+                DeliveryFee = 49,
+                ServiceFee = 0.05m,
+                TotalPrice = 200m,
+                OrderStatus = OrderStatusEnum.Received,
+                OrderItems = new List<OrderItem>
+                {
+                    new OrderItem(1, 2)
+                }
+            },   
+            new Order
+           {
+                Customer = new Customer { Name = "Alfredo", Email = "Alfredo@mail.com", PhoneNum = "0702345678" },
+                Instructions = "Ingen lök tack",
+                DeliveryFee = 49,
+                ServiceFee = 0.05m,
+                TotalPrice = 200m,
+                OrderStatus = OrderStatusEnum.Received,
+                OrderItems = new List<OrderItem>
+                {
+                    new OrderItem(2, 4)
+                }
+            }   
+        };
+        await _db.Orders.AddRangeAsync(orders);
+        await _db.SaveChangesAsync();
     }
 }
