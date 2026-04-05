3c6e23b7484225c3784a545e5d07a43ffaac9ea7
Coolastelammetcodes <Martinlam36@gmail.com>
2026-04-05 14:13:57 +0200

    Order, added prop for OrderStatusEnum and also set defaultvalue. Also made Customer nullable instead of creating a new Customer if null

--- src/domain/entities/Order.cs
+++ src/domain/entities/Order.cs
@@ -1,13 +1,15 @@
+using domain.enums;
 namespace domain.entities;
 public class Order
 {
     public Guid Id { get; init; } = Guid.NewGuid(); 
     public List<OrderItem> OrderItems { get; set; } = new();
     public string Instructions { get; set; } = string.Empty;
-    public Customer Customer { get; set; } = new();
+    public Customer? Customer { get; set; }
     public int DeliveryFee { get; set; }
     public decimal ServiceFee { get; set; }
     public decimal TotalPrice { get; set; } 
+    public OrderStatusEnum OrderStatus { get; set; } = OrderStatusEnum.Received;
 
     // TODO Om du hinner lägg in så att användaren kan välja om hen vill spara sina uppgifter och först då använda Customertabellen men framtills dess behåll så att alla som lägger order sparas 
 }
