185cf2e29b965c0cc28d90d685265ec4d38d7f06
Coolastelammetcodes <Martinlam36@gmail.com>
2026-04-05 17:21:52 +0200

    IOrderRepo and IOrderService, added method for updating OrderStatus in interface

--- src/domain/interfaces/IRepositories/IOrderRepository.cs
+++ src/domain/interfaces/IRepositories/IOrderRepository.cs
@@ -1,8 +1,10 @@
 using domain.entities;
+using domain.enums;
 
 namespace domain.interfaces;
 public interface IOrderRepository
 {
     Task AddNewOrderAsync(Order order);
     Task<Order?> ViewSpecificOrderWithDishesAsync(Guid Id);
+    Task UpdateOrderStatusAsync(Guid id, OrderStatusEnum orderStatus);
 }
--- src/domain/interfaces/IServices/IOrderService.cs
+++ src/domain/interfaces/IServices/IOrderService.cs
@@ -1,7 +1,9 @@
 using domain.dto;
+using domain.enums;
 
 public interface IOrderService
 {
     Task<OrderResponseDTO> AddNewOrderAsync(OrderRequestDTO orderDTO);
     Task<OrderResponseDTO?> ViewSpecificOrderWithDishesAsync(Guid Id);
+    Task<OrderResponseDTO> UpdateOrderStatusAsync(Guid id, OrderStatusEnum orderStatus);
 }
