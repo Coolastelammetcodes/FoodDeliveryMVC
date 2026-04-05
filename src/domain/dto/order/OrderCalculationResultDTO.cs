6c73fcfe0cada21f5bc65c886b18966128419298
Coolastelammetcodes <Martinlam36@gmail.com>
2026-04-05 11:49:58 +0200

    OrderCalculationResultDTO, added props for DeliveryFee and ServiceFee

--- src/domain/dto/order/OrderCalculationResultDTO.cs
+++ src/domain/dto/order/OrderCalculationResultDTO.cs
@@ -4,5 +4,7 @@ using domain.entities;
 public class OrderCalculationResultDTO
 {
     public List<OrderItem> OrderItems { get; set; } = new();
+    public int DeliveryFee { get; set; }
+    public decimal ServiceFee { get; set; }
     public decimal TotalPrice { get; set; }
 }
