c140039e324b2bb5cf18048c452d203a11025579
Coolastelammetcodes <Martinlam36@gmail.com>
2026-04-05 10:58:18 +0200

    DishRepo, included Restaurant in query

--- src/infrastructure/repositories/DishRepository.cs
+++ src/infrastructure/repositories/DishRepository.cs
@@ -10,9 +10,9 @@ public class DishRepository : IDishRepository
     {
         _db = db;
     }
-    public async Task<List<Dish>> GetDishesForRestaurantAsync(int restaurantID) => await _db.Dishes.Where(d => d.RestaurantID == restaurantID).ToListAsync();
+    public async Task<List<Dish>> GetDishesForRestaurantAsync(int restaurantID) => await _db.Dishes.Where(d => d.RestaurantID == restaurantID).Include(d => d.Restaurant).ToListAsync();
     public async Task<List<Dish>> ViewAllDishesAsync() => await _db.Dishes.ToListAsync(); 
-    public async Task<Dish?> ViewSpecificDishAsync(int id) => await _db.Dishes.FindAsync(id);
+    public async Task<Dish?> ViewSpecificDishAsync(int id) => await _db.Dishes.Include(d => d.Restaurant).FirstOrDefaultAsync(d => d.Id == id);
     public async Task AddNewDishAsync(Dish dish)
     {
         await _db.Dishes.AddAsync(dish);
