280b93de2be392377b66abbb39ca3592e9f3172f
Coolastelammetcodes <Martinlam36@gmail.com>
2026-04-06 00:03:58 +0200

    DbInit, added if-statement to check if Orders in database is empty and if it is, use seed-data/orders

--- src/infrastructure/data/DbInitializer.cs
+++ src/infrastructure/data/DbInitializer.cs
@@ -75,36 +75,39 @@ public class DbInitializer
     }
     public async Task SeedOrdersAsync()
     {
-        var orders = new List<Order>
+        if(!await _db.Orders.AnyAsync())
         {
-            new Order
-           {
-                Customer = new Customer { Name = "Alice", Email = "alice@mail.com", PhoneNum = "0701234567" },
-                Instructions = "Ingen lök tack",
-                DeliveryFee = 49,
-                ServiceFee = 0.05m,
-                TotalPrice = 200m,
-                OrderStatus = OrderStatusEnum.Received,
-                OrderItems = new List<OrderItem>
+            var orders = new List<Order>
+            {
+                new Order
                 {
-                    new OrderItem(1, 2)
-                }
-            },   
-            new Order
-           {
-                Customer = new Customer { Name = "Alfredo", Email = "Alfredo@mail.com", PhoneNum = "0702345678" },
-                Instructions = "Ingen lök tack",
-                DeliveryFee = 49,
-                ServiceFee = 0.05m,
-                TotalPrice = 200m,
-                OrderStatus = OrderStatusEnum.ReadyForPickup,
-                OrderItems = new List<OrderItem>
+                    Customer = new Customer { Name = "Alice", Email = "alice@mail.com", PhoneNum = "0701234567" },
+                    Instructions = "Ingen lök tack",
+                    DeliveryFee = 49,
+                    ServiceFee = 0.05m,
+                    TotalPrice = 200m,
+                    OrderStatus = OrderStatusEnum.Received,
+                    OrderItems = new List<OrderItem>
+                    {
+                        new OrderItem(1, 2)
+                    }
+                },   
+                new Order
                 {
-                    new OrderItem(2, 4)
-                }
-            }   
-        };
-        await _db.Orders.AddRangeAsync(orders);
-        await _db.SaveChangesAsync();
+                    Customer = new Customer { Name = "Alfredo", Email = "Alfredo@mail.com", PhoneNum = "0702345678" },
+                    Instructions = "Ingen lök tack",
+                    DeliveryFee = 49,
+                    ServiceFee = 0.05m,
+                    TotalPrice = 200m,
+                    OrderStatus = OrderStatusEnum.ReadyForPickup,
+                    OrderItems = new List<OrderItem>
+                    {
+                        new OrderItem(2, 4)
+                    }
+                }   
+            };
+            await _db.Orders.AddRangeAsync(orders);
+            await _db.SaveChangesAsync();
+        }
     }
 }
