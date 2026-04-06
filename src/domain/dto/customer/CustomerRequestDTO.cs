379b5ee9a080077707382c39ef0bd8be6055e5ce
Coolastelammetcodes <Martinlam36@gmail.com>
2026-04-06 12:59:53 +0200

    CustomerRequestDTO,  new property for lastname and also validation for F-and LName and also PhoneNum

--- src/domain/dto/customer/CustomerRequestDTO.cs
+++ src/domain/dto/customer/CustomerRequestDTO.cs
@@ -3,10 +3,20 @@ using domain.dto;
 
 public class CustomerRequestDTO
 {
-    [Required(ErrorMessage = "Vänligen fyll i namn för att lägga beställning")]
-    public string Name { get; set; } = string.Empty;
+    [Required]
+    [MinLength(2, ErrorMessage = "Förnamn måste vara minst 2 tecken")]
+    [MaxLength(50, ErrorMessage = "Förnamn får innehålla högst 50 tecken")]
+    public string FName { get; set; } = string.Empty;
+    
+    [Required]
+    [MinLength(2, ErrorMessage = "Efternamn måste vara minst 2 tecken")]
+    [MaxLength(50, ErrorMessage = "Efternamn får innehålla högst 50 tecken")]
+    public string LName { get; set; } = string.Empty;
+    
     [Required(ErrorMessage = "Vänligen fyll i telefon-nummer för att lägga beställning")]
+    [RegularExpression(@"^(\+46|0)7\d{8}$")]
     public string PhoneNum { get; set; } = string.Empty;
+    
     [Required(ErrorMessage = "Vänligen fyll i email-address för att lägga beställning")]
     public string Email { get; set; } = string.Empty;
 }
