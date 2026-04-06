7f2e2ea3e1d6ab52053972c5d55f09b6e2730826
Coolastelammetcodes <Martinlam36@gmail.com>
2026-04-06 17:27:44 +0200

    Customer, deleted the List of orders because it will not be relevant yet

--- src/domain/entities/Customer.cs
+++ src/domain/entities/Customer.cs
@@ -7,5 +7,4 @@ public class Customer
     public string LName { get; set; } = string.Empty;
     public string PhoneNum { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
-    public List<Order?> Orders { get; set; } = new(); // används för att hämta ut alla ordrar som tillhör en specifik kund
 }
