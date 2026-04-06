8e03a5d2339215146f20d706327d322080bab36c
Coolastelammetcodes <Martinlam36@gmail.com>
2026-04-07 01:03:13 +0200

    UnitTest1, made a mock for OrderREpo because OrderService  dependancy to it and OrderRepo is relevant for the test. Also finished the rest of the test

--- tests/unitTests/UnitTest1.cs
+++ tests/unitTests/UnitTest1.cs
@@ -10,17 +10,34 @@ public class UnitTest1
     [Fact]
     public async Task Should_Return_Correct_TotalPrice()
     {
-        var mockDishRepo = new Mock<IDishRepository>();
+        var mockOrderRepo = new Mock<IOrderRepository>();
+        mockOrderRepo.Setup(or => or.AddNewOrderAsync(It.IsAny<Order>())).Returns(Task.CompletedTask);
 
+        var mockDishRepo = new Mock<IDishRepository>();
         var dish = new Dish{
             Id = 1,
             Price = 105,
             Restaurant = new Restaurant
             {
-            Id = 3,
-            DeliveryFee = 69,
-            ServiceFee = 0.1m  
+                Id = 3,
+                DeliveryFee = 69,
+                ServiceFee = 0.05m  
             }
         };
+            mockDishRepo.Setup(dr => dr.ViewSpecificDishAsync(1)).ReturnsAsync(dish);
+
+        var orderService = new OrderService(mockOrderRepo.Object, mockDishRepo.Object, null);
+
+        var orderDto = new OrderRequestDTO
+        {
+            OrderItems = new List<OrderItemRequestDTO>
+            {
+                new OrderItemRequestDTO {DishID = 1, Quantity = 4}
+            } 
+        };
+
+        var act = await orderService.AddNewOrderAsync(orderDto);
+
+        Assert.Equal(510, act.TotalPrice);
     }
 }
