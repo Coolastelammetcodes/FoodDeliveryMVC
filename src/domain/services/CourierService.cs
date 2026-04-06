9875090833c89c6f15ad652f3aed95dc2dce94d3
Coolastelammetcodes <Martinlam36@gmail.com>
2026-04-06 17:34:01 +0200

    CourierService, new methods for adding and viewing all couriers. Also a help method for mapping to courierResponseDTO

--- src/domain/services/CourierService.cs
+++ src/domain/services/CourierService.cs
@@ -8,15 +8,33 @@ public class CourierService : ICourierService
         _courierRepo = courierRepo;
     }
 
-    public async Task AddNewCourierAsync(CourierRequestDTO reqDTO)
+    public async Task<CourierResponseDTO> AddNewCourierAsync(CourierRequestDTO reqDTO)
     {
+        var courierPhone = await _courierRepo.GetCourierByPhoneNumAsync(reqDTO.PhoneNum);
+        if (courierPhone != null)
+        {
+            throw new Exception("Ett annat bud har redan detta telefonnummer");    
+        }
+
         var newCourier = new Courier
         {
             FName = reqDTO.FName,
             LName = reqDTO.LName,
             PhoneNum = reqDTO.PhoneNum
         };
-
         await _courierRepo.AddNewCourierAsync(newCourier);
+        return MapToCourierResponseDTO(newCourier);
     }
+    public async Task<List<CourierResponseDTO>> ViewAllCouriersAsync()
+    {
+        var couriers = await _courierRepo.ViewAllCouriersAsync();
+        return couriers.Select(MapToCourierResponseDTO).ToList();
+    }
+    private CourierResponseDTO MapToCourierResponseDTO(Courier courier) => new CourierResponseDTO{
+                                    Id = courier.Id,
+                                    FName = courier.FName,
+                                    LName = courier.LName,
+                                    PhoneNum = courier.PhoneNum,
+                                    IsAvailable = courier.IsAvailable
+                                }; 
 }
