4c64a5e3a1a3a009de09ce82e6a69bf7871aa8fa
Coolastelammetcodes <Martinlam36@gmail.com>
2026-04-06 17:24:41 +0200

    CourierApiController, two new controller-methods for adding and viewing couriers

--- src/api/controllers/CourierApiController.cs
+++ src/api/controllers/CourierApiController.cs
@@ -2,7 +2,33 @@ using Microsoft.AspNetCore.Mvc;
 
 [ApiController]
 [Route("api/[controller]")]
-public class CourierApiController
+public class CourierApiController : ControllerBase
 {
-    // TODO fortsätt här
+    private readonly ICourierService _courierService;
+    public CourierApiController(ICourierService courierService)
+    {
+        _courierService = courierService;
+    }
+    [HttpPost]
+    public async Task<IActionResult> AddNewCourierAsync(CourierRequestDTO reqDTO)
+    {   
+        try
+        {
+            await _courierService.AddNewCourierAsync(reqDTO);
+            return Created();
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        } 
+    }
+    [HttpGet]
+    public async Task<IActionResult> ViewAllCouriersAsync()
+    {
+        var couriers = await _courierService.ViewAllCouriersAsync();
+        
+        if(couriers == null) return NotFound();
+        
+        return Ok(couriers);
+    }
 }
