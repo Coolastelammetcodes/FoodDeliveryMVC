d1295788d89e21baccbc859f89063dab07f15a48
Coolastelammetcodes <Martinlam36@gmail.com>
2026-04-06 23:47:02 +0200

    Dish, deleted constructor

--- src/domain/entities/Dish.cs
+++ src/domain/entities/Dish.cs
@@ -7,13 +7,5 @@ public class Dish
     public string Description { get; set; } 
     public int Price { get; set; }
     public int RestaurantID { get; set; }
-    public Restaurant? Restaurant { get; set; }
-
-    public Dish(string name, string description, int price, int restaurantID)
-    {
-        Name = name;
-        Description = description;
-        Price = price;
-        RestaurantID = restaurantID;
-    }
+    public Restaurant? Restaurant { get; set; } 
 }
