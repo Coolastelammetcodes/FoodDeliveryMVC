9563c1d8a79923b22ceb6102675073be6e0f1d3c
Coolastelammetcodes <Martinlam36@gmail.com>
2026-04-06 23:53:35 +0200

    UnitTest1, beginning of unit test where it will test if OrderService returns correct totalprice.

---
+++ tests/unitTests/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using Moq;
+using domain.interfaces;
+using domain.entities;
+using domain.dto;
+
+namespace unitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public async Task Should_Return_Correct_TotalPrice()
+    {
+        var mockDishRepo = new Mock<IDishRepository>();
+
+        var dish = new Dish{
+            Id = 1,
+            Price = 105,
+            Restaurant = new Restaurant
+            {
+            Id = 3,
+            DeliveryFee = 69,
+            ServiceFee = 0.1m  
+            }
+        };
+    }
+}
