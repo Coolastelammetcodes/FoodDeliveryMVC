db7b1e873f0a49abefd20bd3d56456f3dc270b95
Coolastelammetcodes <Martinlam36@gmail.com>
2026-04-06 12:58:53 +0200

    Customer, added an extra property for lastname

--- src/domain/entities/Customer.cs
+++ src/domain/entities/Customer.cs
@@ -3,7 +3,8 @@ using domain.entities;
 public class Customer
 {
     public Guid Id { get; set; } = Guid.NewGuid();
-    public string Name { get; set; } = string.Empty;
+    public string FName { get; set; } = string.Empty;
+    public string LName { get; set; } = string.Empty;
     public string PhoneNum { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
     public List<Order?> Orders { get; set; } = new(); // används för att hämta ut alla ordrar som tillhör en specifik kund
