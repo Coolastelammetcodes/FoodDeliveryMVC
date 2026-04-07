54be3e6f48930d8260d0ad3d1f2e6813d4dbfe5c
Coolastelammetcodes <Martinlam36@gmail.com>
2026-04-07 03:41:59 +0200

    Restaurant, made string props to empty strings by default

--- src/domain/entities/Restaurant.cs
+++ src/domain/entities/Restaurant.cs
@@ -3,9 +3,9 @@ namespace domain.entities;
 public class Restaurant
 {
     public int Id { get; set; }
-    public string Name { get; set; }
-    public string Address { get; set; }
-    public string Description { get; set; } 
+    public string Name { get; set; } = string.Empty;
+    public string Address { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
     public TimeSpan Open { get; set; }
     public TimeSpan Closed { get; set; }
     public TimeSpan OrderDeadline { get; set; }
