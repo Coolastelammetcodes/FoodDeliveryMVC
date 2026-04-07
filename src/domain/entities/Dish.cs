348c919e21b84a6a0deda11b06fae871b15ccc84
Coolastelammetcodes <Martinlam36@gmail.com>
2026-04-07 03:41:03 +0200

    Dish, made name and description to empty strings by default

--- src/domain/entities/Dish.cs
+++ src/domain/entities/Dish.cs
@@ -3,8 +3,8 @@ namespace domain.entities;
 public class Dish
 {
     public int Id { get; set; }
-    public string Name { get; set; } 
-    public string Description { get; set; } 
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
     public int Price { get; set; }
     public int RestaurantID { get; set; }
     public Restaurant? Restaurant { get; set; } 
